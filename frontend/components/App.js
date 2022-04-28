6392d5de8fdcf8c964027074f88ec28603e8f367
Benjamin F <84413548+bforrest-pct@users.noreply.github.com>
2022-04-28 15:38:28 -0700

    wiring up todo postings

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -7,6 +7,13 @@ export default class App extends React.Component {
   state={
     todos: [],//initialize state with empty array of todos then GET the todos
     error: '',//create state for error
+    todoNameInput: '',//drive input using this state
+  }
+  //helper for todoNameInput
+  onTodoNameInputChange = evt =>{
+    const {value} = evt.target//extract what you need from the data when dealing with targets, then you can deal with the setState.
+    //debugger//using debuggers for breakpoints to make sure that your code is working as intended and will break where you place debugger.
+    this.setState({...this.state, todoNameInput: value })//see note above about putting value in
   }
   //helper function for fetching all todos:
   fetchAllTodos = () =>{
@@ -39,7 +46,7 @@ export default class App extends React.Component {
             }
         </div>
         <form id="todoForm">
-          <input type="text" placeholder="Type your todo here"></input>
+          <input value={this.state.todoNameInput} onChange={this.onTodoNameInputChange} type="text" placeholder="Type your todo here"></input>
           <input type="submit"></input>
           <button>Clear Completed</button>
         </form>
