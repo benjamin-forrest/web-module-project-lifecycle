4888f099504c855ffe61afa2671958c147916298
Benjamin F <84413548+bforrest-pct@users.noreply.github.com>
2022-04-28 14:41:38 -0700

    setup wireframe

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -3,18 +3,24 @@ import React from 'react'
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      
-    }
-    this.componentDidMount(){
-      this.setState({
-
-      })
-    }
+  state={
+    todos: [],//initialize state with empty array of todos then GET the todos
   }
   render() {
-    return null
+    return (
+      <div>
+        <div id="error">Error: no errors here</div>
+        <div id="todos">
+          <h2>Todos:</h2>
+          <div>Walk the dog</div>
+          <div>Learn React</div>
+        </div>
+        <form id="todoForm">
+          <input type="text" placeholder="Type your todo here"></input>
+          <input type="submit"></input>
+          <button>Clear Completed</button>
+        </form>
+      </div>
+    )
   }
 }
