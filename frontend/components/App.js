d2186e2e0f7be46d3f15a543c041257bd32cae88
Benjamin F <84413548+bforrest-pct@users.noreply.github.com>
2022-04-28 13:56:08 -0700

    constructor app

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -3,6 +3,17 @@ import React from 'react'
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      
+    }
+    this.componentDidMount(){
+      this.setState({
+
+      })
+    }
+  }
   render() {
     return null
   }
